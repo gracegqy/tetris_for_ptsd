f76699e8af2f7937e8efdb3c0d3640d58d0b8742
RedPanda41 <35614979+RedPanda41@users.noreply.github.com>
2026-02-23 12:51:15 -0600

    Create tetromino_setup.gd
    My apologies, the previous code I included wasn't compatible with the newest version of Godot. This code should work at setting up the tetrominos before they fall. Grace, let me know if this works compared to what you have set up so far.

---
+++ tetromino_setup.gd
@@ -0,0 +1,95 @@
+var i_tetromino: Array = [
+    [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(3, 1)], # 0 degrees
+    [Vector2i(2, 0), Vector2i(2, 1), Vector2i(2, 2), Vector2i(2, 3)], # 90 degrees
+    [Vector2i(0, 2), Vector2i(1, 2), Vector2i(2, 2), Vector2i(3, 2)], # 180 degrees
+    [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(1, 3)]  # 270 degrees
+]
+ 
+var t_tetromino: Array = [
+    [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+    [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)], # 90 degrees
+    [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)], # 180 degrees
+    [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]  # 270 degrees
+]
+ 
+var o_tetromino: Array = [
+    [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)], # All rotations are the same
+    [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)], # All rotations are the same
+    [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)], # All rotations are the same
+    [Vector2i(0, 0), Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1)]  # All rotations are the same
+]
+ 
+var z_tetromino: Array = [
+    [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+    [Vector2i(2, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(1, 2)], # 90 degrees
+    [Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)], # 180 degrees
+    [Vector2i(1, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(0, 2)]  # 270 degrees
+]
+ 
+var s_tetromino: Array = [
+    [Vector2i(1, 0), Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1)], # 0 degrees
+    [Vector2i(1, 0), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)], # 90 degrees
+    [Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2), Vector2i(1, 2)], # 180 degrees
+    [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(1, 2)]  # 270 degrees
+]
+ 
+var l_tetromino: Array = [
+    [Vector2i(2, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+    [Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2), Vector2i(2, 2)], # 90 degrees
+    [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(0, 2)], # 180 degrees
+    [Vector2i(0, 0), Vector2i(1, 0), Vector2i(1, 1), Vector2i(1, 2)]  # 270 degrees
+]
+ 
+var j_tetromino: Array = [
+    [Vector2i(0, 0), Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1)], # 0 degrees
+    [Vector2i(1, 0), Vector2i(2, 0), Vector2i(1, 1), Vector2i(1, 2)], # 90 degrees
+    [Vector2i(0, 1), Vector2i(1, 1), Vector2i(2, 1), Vector2i(2, 2)], # 180 degrees
+    [Vector2i(1, 0), Vector2i(1, 1), Vector2i(0, 2), Vector2i(1, 2)]  # 270 degrees
+]
+
+var tetrominoes: Array = [i_tetromino, t_tetromino, o_tetromino, z_tetromino, s_tetromino, l_tetromino, j_tetromino]
+var all_tetrominoes: Array = tetrominoes.duplicate()
+
+const COLS: int = 10
+const ROWS: int = 20
+
+const START_POSITION: Vector2i + Vector2i(5, 1)
+var current_position: Vector2i
+
+var current_tetromino_type: Array
+var next_tetromino_type: Array
+var rotation_index: int = 0
+var active_tetromino: Array = []
+
+var tile_id: int = 0
+var pece_atlas: Vector2i
+var next_piece_atlas: Vector2i
+
+@onready var board_layer: TileMapLayer = $Board ##Replace with whatever you called the board node
+@onready var active_layer: TileMapLayer = $Active ##Replace with whatever you called the active piece node
+
+func _ready() -> void:
+  start_new_game()
+
+func start_new_game() -> void:
+  current_tetromino_type = choose_tetromino()
+
+func choose_tetromino() -> Array:
+  var selected_tetromin: Array
+  if not tetrominos.is_empty():
+    tetrominoes.shuffle()
+    selected_tetromino = tetrominoes.pop_front()
+  else:
+    tetrominoes = all_tetrominoes.duplicate()
+    tetrominoes.shuffle()
+    selected_tetromino = tetrominoes.pop_front()
+  return selcted_tetromino
+
+func initialize_tetromino() -> void:
+  current.position = START_POSITION
+  active.tetromino = current_tetromino_type[rotation_index]
+  render_tetromino(active_tetromino, current_position, piece_atlas)
+
+func render_tetromino(tetromino: Array, position: Vector2i, atlas: Vector2i) -> void:
+  for block in tetromino:
+    board_layer.set_cell(position + block, tile_id, atlas)
