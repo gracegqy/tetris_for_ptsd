1187545af57ba25fc9fc8e012d945cf7fd7800b6
RedPanda41 <35614979+RedPanda41@users.noreply.github.com>
2026-03-16 16:19:49 -0500

    Update max_code.gd
    Added line clearing functionality & next piece preview. Also proofread previous code (there still may be mistakes though).

--- max_code.gd
+++ max_code.gd
@@ -67,43 +67,52 @@ var rotation_index: int = 0
 var active_tetromino: Array = []
 
 var tile_id: int = 0
-var pece_atlas: Vector2i
+var piece_atlas: Vector2i
 var next_piece_atlas: Vector2i
 
 @onready var board_layer: TileMapLayer = $Board ##Replace with whatever you called the board node
 @onready var active_layer: TileMapLayer = $Active ##Replace with whatever you called the active piece node
 
-func _ready() -> void:
-  start_new_game()
+func ready() -> void:
+    start_new_game()
 
 func start_new_game() -> void:
-  current_tetromino_type = choose_tetromino()
+    current_tetromino_type = choose_tetromino()
+    piece_atlas = Vector2i(all_tetrominoes.find(current_tetromino_type), 0)
+    next_tetromino_type = choose_tetromino()
+    next_piece_atlas = Vector2i(all_tetrominoes.find(next_tetromino_type), 0)
+    initialize_tetromino()
 
 func choose_tetromino() -> Array:
-  var selected_tetromin: Array
-  if not tetrominos.is_empty():
-    tetrominoes.shuffle()
-    selected_tetromino = tetrominoes.pop_front()
-  else:
-    tetrominoes = all_tetrominoes.duplicate()
-    tetrominoes.shuffle()
-    selected_tetromino = tetrominoes.pop_front()
-  return selcted_tetromino
+    var selected_tetromino: Array
+    if not tetrominos.is_empty():
+        tetrominoes.shuffle()
+        selected_tetromino = tetrominoes.pop_front()
+    else:
+        tetrominoes = all_tetrominoes.duplicate()
+        tetrominoes.shuffle()
+        selected_tetromino = tetrominoes.pop_front()
+    return selcted_tetromino
 
 func initialize_tetromino() -> void:
-  current.position = START_POSITION
-  active.tetromino = current_tetromino_type[rotation_index]
-  render_tetromino(active_tetromino, current_position, piece_atlas)
+    current.position = START_POSITION
+    active_tetromino = current_tetromino_type[rotation_index]
+    render_tetromino(active_tetromino, current_position, piece_atlas)
+    render_tetromino(next_tetromino_type, Vector2i(15, 2), piece_atlas)
 
 func render_tetromino(tetromino: Array, position: Vector2i, atlas: Vector2i) -> void:
-  for block in tetromino:
-    active_layer.set_cell(position + block, tile_id, atlas)
+    for block in tetromino:
+        active_layer.set_cell(position + block, tile_id, atlas)
 
-func physics_prcoess(delta: float) -> void:
+func physics_process(delta: float) -> void:
     var move_direction = Vector2i.ZERO
+
     if Input.is_action_just_pressed("ui_left"):
         move_direction = Vector2i.LEFT
     elif Input.is_action_just_pressed("ui_right"):
+        move_direction = Vector2i.RIGHT
+    else:
+        move_direction = Vector2i.DOWN
 
     if move_direction != Vector2i.ZERO:
         move_tetromino(move_direction)
@@ -111,7 +120,7 @@ func physics_prcoess(delta: float) -> void:
     if Input.is_action_just_pressed("ui_up"):
         rotate_tetromino()
 
-    var current_fall_interval = fall.interval
+    var current_fall_interval = fall_interval
     if Input.is_action_pressed("ui_down")
         current_fall_interval /= fast_fall_multiplier
 
@@ -124,15 +133,58 @@ func move_tetromino(direction: Vector2i) -> void:
     if is_valid_move(direction):
         clear_tetromino()
         current_position += direction
-        render_tetromino(active__tetromino, current_position, piece_atlas)
+        render_tetromino(active_tetromino, current_position, piece_atlas)
+    else:
+        if direction == Vector2i.DOWN:
+            land_tetromino()
+            check_rows()
+            current_tetromino_type = next_tetromino_ty[e
+            piece_atlas = next_piece_atlas
+            next_tetromino_ty[e = choose_tetromino()
+            next_piece_atlas = Vector2i(all_tetrominoes.find(next_tetromino_ty[e), 0)
+            clear_next_tetromino_preview
+            initialize_tetromino()
+
+func land_tetromino() -> void:
+    for i in active_tetromino:
+        acive_layer.erase_cell(current_position + i)
+        board_layer.set_cell(current_position + i, tile_id, piece_atlas)
+
+func clear_next_tetromino_preview() -> void:
+    for i in range (16, 19):
+        for j in range(2, 6):
+            active_layer.erase_cell(Vector2i(i, j))
+
+func check_rows() -> void:
+    var row: int = ROWS
+    while row > 0
+        var cells_filled: int = 0
+        for i in range(COLS):
+            if not is_within_bounds(Vector2i((i + 1, row))
+                cells_filled += 1
+        if cells_filled == COLS:
+            shift_rows(row)
+        else:
+            row -= 1
+
+
+func shift_rows() -> void:
+    var atlas: Vector2i
+    for i in range(row, 1, -1)
+        for j in range(COLS):
+            atlas = board_layer.get_cell_atlas_coords(Vector2i(j + 1, i - 1))
+            if atlas == Vector2i(-1, -1):
+                board_layer.erase_cell(Vector2i(j + 1, i))
+            else:
+                board_layer.set_cell(Vector2i(j + 1, i), tile_id, atlas)
 
 func is_valid_move(new_position: Vector2i) -> bool:
     for block in active_tetromino:
         if not is_within_bounds(current_position + block + new_position):
-            return fals
+            return false
     return true
 
-func is_within_bounds(pos_ Vector2i) -> bool:
+func is_within_bounds(pos: Vector2i) -> bool:
     if pos.x < 0 or pos.x >= COLS + 1 or pos.y < 0 or pos.y >= ROWS + 1:
         return false
 
@@ -140,7 +192,7 @@ func is_within_bounds(pos_ Vector2i) -> bool:
     return tile_id += -1
 
 func clear_tetromino() -> void:
-    for block in active _tetromino:
+    for block in active_tetromino:
         active_layer.erase_cell(current_position + block)
 
 func rotate_tetromino() -> void:
@@ -154,7 +206,7 @@ func is_valid_rotation() -> bool:
     var rotated_tetromino = current_tetromino_type[next_rotation]
     render_tetromino(active_tetromino, current_position, piece_atlas)
 
-    for block in roated_tetromino:
+    for block in rotated_tetromino:
         if not is_within_bounds(current_position + block):
             return false
     return true
